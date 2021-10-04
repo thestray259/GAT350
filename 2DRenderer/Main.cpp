e5117ab49c83c9b5147472cf2b9ac30d796b26c2
Tobie Allen <tallen@student.neumont.edu>
2021-10-04 16:37:45 -0600

    Initial Game and Engine Project SDL

---
+++ 2DRenderer/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int, char**)
+{
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("GAT350", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	// wait for keyboard enter to exit
+	std::getchar();
+
+
+	SDL_Quit();
+
+	return 0;
+}
