10f49082732ba86a40679cf045d7c0e63a88a57c
Tobie Allen <tallen@student.neumont.edu>
2021-11-01 09:40:28 -0600

    Added cross product

--- RayTracer/Types.h
+++ RayTracer/Types.h
@@ -70,3 +70,12 @@ inline glm::vec3 randomInUnitDisk()
 
     return p;
 }
+
+inline glm::vec3 cross(const glm::vec3& v1, const glm::vec3& v2)
+{
+    return glm::vec3{
+        v1.y * v2.z - v1.z * v2.y,
+        v1.z * v2.x - v1.x * v2.z,
+        v1.x * v2.y - v1.y * v2.x
+    };
+}
